1f60dd1cbe903f9208a3f324d1f56c70b48f286f
Steve <pickleman20@hotmail.com>
2020-12-05 02:55:04 -0500

    initialized MuseumInfoActivity & MuseumsActivity & setup basic setup for both xml files

---
+++ src/app/src/main/java/com/example/museums/MuseumInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.museums;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MuseumInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.museum_info);
+    }
+}
