1c467f755adeb25ef9b5140dccdeaaa58158c190
JulianRomeroSD <61603388+JulianRomeroSD@users.noreply.github.com>
2020-12-09 21:43:31 -0500

    Update MuseumsActivity.java

--- src/app/src/main/java/com/example/museums/MuseumsActivity.java
+++ src/app/src/main/java/com/example/museums/MuseumsActivity.java
@@ -1,7 +1,8 @@
-package com.example.museums;
+package com.example.museumapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 /**
@@ -9,14 +10,28 @@ import androidx.appcompat.app.AppCompatActivity;
  * @author Steven Nguyen, Julian Romero
  */
 public class MuseumsActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.museums); //temp disabled to test MuseumInfoActivity - enable this and remove below later
-
-        Intent intent = new Intent(this, MuseumInfoActivity.class);
-        intent.putExtra("museumName", "Metropolitan Museum of Art");
-        startActivity(intent);
+        if(savedInstanceState.equals("Metropolitan Museum of Art")){
+            Intent intent = new Intent(this, MuseumInfoActivity.class);
+            intent.putExtra("Metropolitan Museum of Art", "Metropolitan Museum of Art");
+            startActivity(intent);
+        }
+        else if(savedInstanceState.equals("Guggenheim Museum")){
+            Intent intent1 = new Intent(this, MuseumInfoActivity.class);
+            intent1.putExtra("Guggenheim Museum", "Guggenheim Museum");
+            startActivity(intent1);
+        }
+        else if(savedInstanceState.equals("American Museum of Natural History")){
+            Intent intent2 = new Intent(this, MuseumInfoActivity.class);
+            intent2.putExtra("American Museum of Natural History", "American Museum of Natural History");
+            startActivity(intent2);
+        }
+        else{
+            Intent intent3 = new Intent(this, MuseumInfoActivity.class);
+            intent3.putExtra("Museum of Modern Art", "Museum of Modern Art");
+            startActivity(intent3);
+        }
     }
 }
